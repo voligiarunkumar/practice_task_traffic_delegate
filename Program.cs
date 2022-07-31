6180c67789d4f1f98733f17a5586263bee910a5c
arunkumar <voligiarunkumar9912345@gmail.com>
2022-07-31 19:18:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace cn_traffic_delegate
+{
+    internal class Program
+    {
+        public delegate void TrafficDel();
+        class TrafficSignall
+        {
+
+            public static void signals() { Console.WriteLine("This are the signals"); }
+            public static void Red() { Console.WriteLine("Red light signals "); }
+
+            public static void Green() { Console.WriteLine("Green light signals "); }
+
+            public static void yellow() { Console.WriteLine("Yellow light signals "); }
+
+            TrafficDel[] obj = new TrafficDel[4];
+            public void IdentifySignal()
+            {
+                obj[0] = new TrafficDel(signals);
+                obj[2] = new TrafficDel(yellow);
+
+                obj[1] = new TrafficDel(Green);
+                obj[3] = new TrafficDel(Red);
+            }
+            public void show()
+            {
+                obj[0]();
+                Console.WriteLine("-----------------------------------");
+                obj[1]();
+                obj[2]();
+                obj[3]();
+
+            }
+            static void Main(string[] args)
+            {
+                TrafficSignall trafficSignall = new TrafficSignall();
+                trafficSignall.IdentifySignal();
+                trafficSignall.show();
+            }
+
+
+        }
+    }
+}
